7ccd7f8553a0217799ace498036262edd0730424
Fatih-Tarim <fatihtrm99@gmail.com>
2022-12-17 22:54:33 +0300

    2

--- app/class.py
+++ app/class.py
@@ -8,16 +8,39 @@ class Person:
         self.name = name
         self.year = year
         
-        #methods
+    # instance methods
+    def intro(self):
+        print("Hi there. I'm " +self.name)
+
+    # instance methods
+    def calculateAge(self):
+        return 2022 - self.year
 
 # object (instance)
-p1 = Person(name='fatih',year='1999')
-p2 = Person('burak','1995')
+p1 = Person(name='fatih',year=1999)
+p2 = Person('burak',1995)
 
 #updating
 p1.name = 'ahmet'
 
-#accessing object attributes
-print(f"p1 - name:{p1.name} year:{p1.year} address: {p1.address}")
-print(f"p2 - name:{p2.name} year:{p2.year}")
+p1.intro()
 
+print(f'age: {p1.calculateAge()}')
+
+
+class Circle:
+    #Class object attribute
+    pi = 3.14
+
+    def __init__(self, yaricap=1):
+        self.yaricap = yaricap
+    
+    #Methods
+    def cevre_hesapla(self):
+        return 2 * self.pi * self.yaricap
+    def alan_hesapla(self):
+        return self.pi * (self.yaricap**2)
+c1 = Circle()
+c2 = Circle(5)
+print(f'c1: alan = {c1.alan_hesapla()} çevre = {c1.cevre_hesapla()}')
+print(f'c2: alan = {c2.alan_hesapla()} çevre = {c2.cevre_hesapla()}')
